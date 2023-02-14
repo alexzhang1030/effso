1a1ae0a366f484a37d02898dff3e2278829afdd4
alexzhang1030 <1642114555@qq.com>
2023-02-14 21:26:08 +0800

    fix: hotfix

--- src/commands/setup.ts
+++ src/commands/setup.ts
@@ -8,7 +8,7 @@ import { DEFAULT_TEMPLATE_PATH } from '../utils/path'
 const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export const setup = async () => {
-  const fromPath = await packageDirectory() ?? __dirname
+  const fromPath = await packageDirectory({ cwd: __dirname }) ?? __dirname
   const targetPath = `${DEFAULT_TEMPLATE_PATH}/example`
   copy(resolve(fromPath, './template/example'), targetPath)
   consola.success(`Effso template copied to ${targetPath}`)
