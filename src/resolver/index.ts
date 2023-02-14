6410737638fa6b2b2a31fc8cb8a3887f8a5afaff
alexzhang1030 <1642114555@qq.com>
2023-02-14 14:47:36 +0800

    refactor: directory architecture

--- src/resolver/index.ts
+++ src/resolver/index.ts
@@ -1,8 +1,7 @@
 import fg from 'fast-glob'
-import { confirm, multiselect, select } from '@clack/prompts'
-import { copy } from 'fs-extra'
-import { GLOB_PATTERNS, joinTemplate, resolveSpecial, specialKeys, specialMapping, withTarget } from '../utils/path'
-import { safetyRun } from '../utils/run'
+import { select } from '@clack/prompts'
+import { joinTemplate } from '../utils/path'
+import { resolveSingle } from './single'
 
 const resolveRootOptions = async (rootDirs: string[] = []) => {
   return await select({
@@ -12,31 +11,6 @@ const resolveRootOptions = async (rootDirs: string[] = []) => {
       label: item,
     })),
   })
-}
-
-const resolveSingle = async (parentPath: string) => {
-  const singleFiles = resolveSpecial(await fg([...GLOB_PATTERNS.single], {
-    onlyFiles: true,
-    cwd: parentPath,
-    dot: true,
-  }))
-  const selected = await multiselect({
-    message: '[Single file]: Pick you want to overrides',
-    options: singleFiles.map(item => ({
-      value: item,
-      label: item,
-    })),
-  }) as string[]
-  const sure = await confirm({
-    message: 'Are you sure you want to overrides these files?',
-  })
-  if (!sure)
-    return
-  safetyRun(async () => await Promise.all(resolveSpecial(selected, false).map((item) => {
-    return copy(joinTemplate(`${parentPath}/${item}`), withTarget(
-      specialKeys.includes(item) ? specialMapping[item as keyof typeof specialMapping] : item,
-    ))
-  })))
 }
 
 export const resolve = async (path: string) => {
---
+++ src/resolver/single.ts
@@ -0,0 +1,30 @@
+import { confirm, multiselect } from '@clack/prompts'
+import fg from 'fast-glob'
+import { copy } from 'fs-extra'
+import { GLOB_PATTERNS, joinTemplate, resolveSpecial, specialKeys, specialMapping, withTarget } from '../utils/path'
+import { safetyRun } from '../utils/run'
+
+export const resolveSingle = async (parentPath: string) => {
+  const singleFiles = resolveSpecial(await fg([...GLOB_PATTERNS.single], {
+    onlyFiles: true,
+    cwd: parentPath,
+    dot: true,
+  }))
+  const selected = await multiselect({
+    message: '[Single file]: Pick you want to overrides',
+    options: singleFiles.map(item => ({
+      value: item,
+      label: item,
+    })),
+  }) as string[]
+  const sure = await confirm({
+    message: 'Are you sure you want to overrides these files?',
+  })
+  if (!sure)
+    return
+  safetyRun(async () => await Promise.all(resolveSpecial(selected, false).map((item) => {
+    return copy(joinTemplate(`${parentPath}/${item}`), withTarget(
+      specialKeys.includes(item) ? specialMapping[item as keyof typeof specialMapping] : item,
+    ))
+  })))
+}
