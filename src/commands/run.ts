2e6e397aa9494730b7f760d51ae92fb4b22a8e9b
alexzhang1030 <1642114555@qq.com>
2023-02-14 21:32:46 +0800

    feat: catch error when don't have any template

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,8 +1,16 @@
 import { intro, outro } from '@clack/prompts'
+import { exists } from 'fs-extra'
+import chalk from 'chalk'
+import consola from 'consola'
 import { resolve } from '../resolver'
 import { DEFAULT_TEMPLATE_PATH } from '../utils/path'
 
 export const run = async () => {
+  const isExist = await exists(DEFAULT_TEMPLATE_PATH)
+  if (!isExist) {
+    consola.error(`Can't find any template in ${chalk.blueBright(DEFAULT_TEMPLATE_PATH)}, try run ${chalk.cyan('effso setup')}`)
+    return
+  }
   intro('🚀 Effso')
   await resolve(DEFAULT_TEMPLATE_PATH)
   outro('👋 Effso end...')
