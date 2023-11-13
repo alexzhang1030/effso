4fa2f76c84aea884243fa2c00eee96556bae0e90
alexzhang1030 <1642114555@qq.com>
2023-11-13 18:03:13 +0800

    feat: support spinner

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -2,7 +2,7 @@ import { resolve } from 'node:path'
 import { readFileSync, rmdirSync } from 'node:fs'
 import { destr } from 'destr'
 import degit from 'degit'
-import { outro } from '@clack/prompts'
+import { outro, spinner } from '@clack/prompts'
 import { targetRoot } from '@/utils'
 
 interface Config {
@@ -15,6 +15,8 @@ export async function build() {
   const jsonPath = resolve(targetRoot(), '.effso/config.json')
   const json = destr<Config>(readFileSync(jsonPath, 'utf-8'))
   if (json.extends) {
+    const s = spinner()
+    s.start('Building...')
     for (const extend of json.extends) {
       await degit(extend, {
         cache: false,
@@ -23,7 +25,9 @@ export async function build() {
         mode: 'tar',
       }).clone(targetRoot())
     }
+    // remove config file? or keep it? need discussion
+    rmdirSync(resolve(targetRoot(), '.effso'), { recursive: true })
+    s.stop()
   }
-  rmdirSync(resolve(targetRoot(), '.effso'))
   outro('Done!')
 }
